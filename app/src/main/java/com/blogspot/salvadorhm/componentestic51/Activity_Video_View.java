d59b1e7ff2874cf68aef36e0c0bd2a4ea5792728
AlexisAndresHR <1717110253@utectulancingo.edu.mx>
2019-03-08 10:57:28 -0600

    Activity VideoView creado correctamente

---
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Video_View.java
@@ -0,0 +1,13 @@
+package com.blogspot.salvadorhm.componentestic51;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Video_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__video__view);
+    }
+}
