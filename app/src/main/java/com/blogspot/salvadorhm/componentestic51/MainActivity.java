4640c2b5ff4302d7dbf6d9ed1383090b4fa42bc0
Salvador Hernández Mendoza <salvadorhm@utec-tgo.edu.mx>
2019-03-08 10:44:15 -0600

    ScrollView Menu actualizado

--- app/src/main/java/com/blogspot/salvadorhm/componentestic51/MainActivity.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/MainActivity.java
@@ -1,5 +1,6 @@
 package com.blogspot.salvadorhm.componentestic51;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,9 +81,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_scroll_view) {
+            Intent intent = new Intent(getApplicationContext(),Activity_Scroll_View.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_edit_text) {
 
         } else if (id == R.id.nav_slideshow) {
 
