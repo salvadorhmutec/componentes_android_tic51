de7035ac136bfc584acf704e63b98f964dc55b8e
Jireh Hernandez Castillo <1717110620@utectulancingo.edu.mx>
2019-03-10 10:58:53 -0600

    FloatingButton creado exitosamente

---
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Floating_Button.java
@@ -0,0 +1,13 @@
+package com.blogspot.salvadorhm.componentestic51;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Floating_Button extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__floating__button);
+    }
+}
