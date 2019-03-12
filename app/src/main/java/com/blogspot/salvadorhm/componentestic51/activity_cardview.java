c05ba5b460adff530165d436e5959c3c007e0b3e
DiegoBP12 <1717110611@utectulancingo.edu.mx>
2019-03-12 00:06:00 -0600

    CardView creado exitosamente

---
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/activity_cardview.java
@@ -0,0 +1,13 @@
+package com.blogspot.salvadorhm.componentestic51;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_cardview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardview);
+    }
+}
