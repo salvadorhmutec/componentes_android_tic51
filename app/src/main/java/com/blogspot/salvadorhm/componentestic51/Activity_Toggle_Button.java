a25f13703f96112c89d85fe4294394d6f56cc6a2
franzruddy <salinasfranzruddy@gmail.com>
2019-03-18 04:42:39 -0400

    creado con exito

--- app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Toggle_Button.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Toggle_Button.java
@@ -2,12 +2,34 @@ package com.blogspot.salvadorhm.componentestic51;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class Activity_Toggle_Button extends AppCompatActivity {
+
+    ToggleButton tgbtn;
+    TextView txEti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__toggle__button);
+        tgbtn=findViewById(R.id.tbBtn1);
+        txEti=(TextView) findViewById(R.id.textView3);
+    }
+    public void onDefaultToggleClick(View view) {
+        Toast.makeText(this, "DefaultToggle", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onToggleClick(View view) {
+        if (view.getId() == R.id.tbBtn1) {
+            if (tgbtn.isChecked()) {
+                txEti.setText("Boton On");
+            } else {
+                txEti.setText("Boton Off");
+            }
+        }
     }
 }
