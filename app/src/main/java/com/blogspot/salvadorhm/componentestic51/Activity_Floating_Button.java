1c9ad8e9b21ab849e6f7156f98c76fb8df60434f
Jireh Hernandez Castillo <1717110620@utectulancingo.edu.mx>
2019-03-19 19:15:46 -0600

    FloatingButton creado exitosamente

--- app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Floating_Button.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Floating_Button.java
@@ -2,12 +2,25 @@ package com.blogspot.salvadorhm.componentestic51;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Activity_Floating_Button extends AppCompatActivity {
+    ImageButton floatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__floating__button);
+
+        floatButton = (ImageButton) findViewById(R.id.imageButton);
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Este es el Telefono de la Universidad 01 771 247 4026", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
