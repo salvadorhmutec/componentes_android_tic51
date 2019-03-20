ed917ed26913eb6ef520c24f01166153355fac72
Israel Merida Lopez <1717110625@utectulancingo.edu.mx>
2019-03-20 13:45:30 -0600

    Actividad Web View Realizado

--- app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Web_View.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Web_View.java
@@ -2,12 +2,18 @@ package com.blogspot.salvadorhm.componentestic51;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Activity_Web_View extends AppCompatActivity {
-
+    private WebView miWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__web__view);
+        miWebView = findViewById(R.id.webviewl);
+        miWebView.getSettings().setJavaScriptEnabled(true);
+        miWebView.setWebViewClient(new WebViewClient());
+        miWebView.loadUrl("https://developer.android.com/guide/webapps/webview");
     }
 }
