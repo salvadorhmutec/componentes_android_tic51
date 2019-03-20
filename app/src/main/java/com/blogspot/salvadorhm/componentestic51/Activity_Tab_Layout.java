a2b67d6dab0b484233d2280575f5dc578ed6a600
Jireh Hernandez Castillo <1717110620@utectulancingo.edu.mx>
2019-03-20 12:24:39 -0600

    ListView creado exitosamente

--- app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Tab_Layout.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Tab_Layout.java
@@ -1,13 +1,45 @@
 package com.blogspot.salvadorhm.componentestic51;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class Activity_Tab_Layout extends AppCompatActivity {
+public class Activity_Tab_Layout extends Activity {
+
+    private String lenguajeProgramacion[]=new String[]{"Java","PHP","Python","JavaScript","Ruby","C",
+            "Go","Perl","Pascal"};
+
+    private Integer[] imgid={
+            R.drawable.java,
+            R.drawable.php,
+            R.drawable.python,
+            R.drawable.javascript,
+            R.drawable.ruby,
+            R.drawable.c,
+            R.drawable.go,
+            R.drawable.perl,
+            R.drawable.pascal
+    };
+
+    private ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__tab__layout);
+
+        LenguajeListAdapter adapter=new LenguajeListAdapter(this,lenguajeProgramacion,imgid);
+        lista=(ListView)findViewById(R.id.mi_lista);
+        lista.setAdapter(adapter);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String Slecteditem= lenguajeProgramacion[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
