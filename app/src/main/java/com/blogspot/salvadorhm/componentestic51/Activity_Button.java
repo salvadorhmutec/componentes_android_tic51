518e0ce29ad303e63df7bbfa7bdef39e959e7a76
agustin monter vera <1717110270@utectulancingo.edu.mx>
2019-03-19 23:01:36 -0600

    Activity_Button terminada exitosamente

--- app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Button.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Button.java
@@ -1,13 +1,27 @@
 package com.blogspot.salvadorhm.componentestic51;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Activity_Button extends AppCompatActivity {
+
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__button);
+
+        btn1= (Button) findViewById(R.id.btnBotonusandooncliCK);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"TIC 51",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
