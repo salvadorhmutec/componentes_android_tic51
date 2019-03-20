09fc3a9dfec2a5e716b4f5eca25fd202f7ac33b9
IvanHernandezOsornio <ivan.herdez@outlook.com>
2019-03-19 22:23:05 -0600

    Spinner Creado Exitosamente

--- app/src/main/java/com/blogspot/salvadorhm/componentestic51/activity_spinner.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/activity_spinner.java
@@ -2,12 +2,46 @@ package com.blogspot.salvadorhm.componentestic51;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class activity_spinner extends AppCompatActivity {
 
+    TextView estado;
+    Spinner combodias;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner);
+
+        //Variables de seleccion
+
+        estado = (TextView) findViewById (R.id.etiSeleccion);
+
+        combodias= (Spinner) findViewById(R.id.spinnerDias);
+
+        //adaptador para llevar la infomacion de opciones a el Spinner
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.combo_dias,android.R.layout.simple_spinner_item
+        );
+
+        combodias.setAdapter(adapter);
+        combodias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(),
+                        "Seleccionado: "+parent.getItemAtPosition(position).toString(),Toast.LENGTH_LONG).show();
+                estado.setText("Dia: "+parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
 }
