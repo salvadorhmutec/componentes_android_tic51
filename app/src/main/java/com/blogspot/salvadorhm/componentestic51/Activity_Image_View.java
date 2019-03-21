81ed1385cf82c79d014263266bfc785cd9b28bf8
RicardoAntonio24 <39196878+RicardoAntonio24@users.noreply.github.com>
2019-03-21 11:27:59 -0600

    Activity_image_view

--- app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Image_View.java
+++ app/src/main/java/com/blogspot/salvadorhm/componentestic51/Activity_Image_View.java
@@ -1,13 +1,40 @@
-package com.blogspot.salvadorhm.componentestic51;
+ package com.blogspot.salvadorhm.componentestic51;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.widget.Button;
+        import android.widget.ImageView;
+        import android.view.View;
+        import android.view.View.OnClickListener;
 
-public class Activity_Image_View extends AppCompatActivity {
+public class Activity_Image_View extends Activity {
+
+    Button button;
+    ImageView image;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__image__view);
+
+        addListenerOnButton();
+
     }
+
+    public void addListenerOnButton() {
+
+        image = (ImageView) findViewById(R.id.imageView1);
+
+        button = (Button) findViewById(R.id.btnCambiarImagen);
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                image.setImageResource(R.drawable.imageView10);
+            }
+
+        });
+
+    }
+
 }
